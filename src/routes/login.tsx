80e0f9bfcf3d2ca0e4bf8f3a9af0ce3ddd1b723f
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-05-24 12:35:55 +0200

    prod tg bot

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -33,7 +33,7 @@ function LoginPage() {
     const script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-widget.js?22'
     script.async = true
-    script.setAttribute('data-telegram-login', 'DeabdfaaeacBot')
+    script.setAttribute('data-telegram-login', import.meta.env.VITE_TELEGRAM_BOT_NAME ?? 'savetherecipe_bot')
     script.setAttribute('data-size', 'large')
     script.setAttribute('data-onauth', 'onTelegramAuth(user)')
     script.setAttribute('data-request-access', 'write')
